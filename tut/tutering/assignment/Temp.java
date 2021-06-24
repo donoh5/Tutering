23134a8a70154b1fb5af61fb2f9008e726413ba2
Don Oh <okdonguk0@gmail.com>
2021-06-23 23:42:12 -0600

    Update Temp.java

--- tut/tutering/assignment/Temp.java
+++ tut/tutering/assignment/Temp.java
@@ -3,14 +3,6 @@ package assignment;
 public class Temp {
    public static void main(String[] args) {
       
-      int i;
-      int j;
-      char a = 'a';
-      for (i=0; i<5; i++) {
-         for (j=0; j<5; j++) {
-            System.out.print((char)((int)a + i));
-            System.out.println((char)((int)a + j));
-         }
-      }
+      System.out.println((int)'A');
    }
 }
